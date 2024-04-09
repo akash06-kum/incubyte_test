80833bdba71cddafc8979f9a9aab5d0b8ee721b2
Akash Kumrawat <akash.kumrawat.sdbg@gmail.com>
2024-04-09 15:35:20 +0530

    Complete solution

--- app/models/sum.rb
+++ app/models/sum.rb
@@ -1,7 +1,15 @@
 class Sum < ApplicationRecord
   def self.add(numbers)
     return 0 if numbers.empty?
-    nums = numbers.split(",").map(&:to_i)
-    nums.sum
+  
+    delimiter = ','
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.match(/\/\/(.)\n(.*)/).captures
+    end
+  
+    negatives = numbers.split(/#{delimiter}|\n/).map(&:to_i).select { |num| num.negative? }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+  
+    numbers.split(/#{delimiter}|\n/).map(&:to_i).reject { |num| num > 1000 }.sum
   end
 end
--- spec/models/sum_spec.rb
+++ spec/models/sum_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe Sum, type: :model do
   it 'returns the sum of numbers for comma-separated numbers' do
     expect(Sum.add("1,5")).to eq(6)
   end
+
+  it 'returns the sum of numbers for numbers separated by comma and newline' do
+    expect(Sum.add("1\n2,3")).to eq(6)
+  end
+
+  it 'returns the sum of numbers for custom delimiter' do
+    expect(Sum.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'raises an error for negative numbers' do
+    expect { Sum.add("1,-2,3") }.to raise_error("Negative numbers not allowed: -2")
+  end
+
+  it 'raises an error for multiple negative numbers' do
+    expect { Sum.add("-1,-2,-3") }.to raise_error("Negative numbers not allowed: -1, -2, -3")
+  end
+
+  it 'ignores numbers greater than 1000' do
+    expect(Sum.add("2,1001")).to eq(2)
+  end
 end
